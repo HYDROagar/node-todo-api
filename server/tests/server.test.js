4e4f1f5e754c801892865444f28ad4c59ecdcadd
Mitchell Eimermann <mitch@Mitchells-MacBook-Pro.local>
2018-04-03 14:38:41 -0500

    DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -132,6 +132,37 @@ describe('PATCH /todos/:id', () => {
         })
         .end(done);
     });
+});
+
+describe('POST / users/login', () => {
+    it('Should login user and return auth token', (done) => {
+        request(app).post('/users/login')
+        .send({
+            email: users[1].email,
+            password: users[1].password
+        })
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toExist();
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            
+            User.findById(users[1]._id).then((user) => {
+                expect(user.tokens[0]).toInclude({
+                    access: 'auth',
+                    token: res.headers['x-auth']
+                });
+                done();
+            }).catch((e) => done(e));
+        })
+    });
+    
+    it('Should reject invalid login', (done) => {
+        
+    })
 })
 
 
